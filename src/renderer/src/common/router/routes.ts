4ee4b69ae2bed126171234c69a7a700d16112550
Youssef Amazzal <20youssefyoussef02@gmail.com>
2025-05-26 15:59:05 +0100

    remove refactor unused css

--- src/renderer/src/common/router/routes.ts
+++ src/renderer/src/common/router/routes.ts
@@ -1,13 +1,21 @@
 const RouteNames = {
   LIBRARY: 'library',
-  COMMUNITY: 'community'
+  COMMUNITY: 'community',
+  MEDIA_PLAYER: 'mediaplayer'
 }
 
 const routes = [
   {
     path: '/',
     name: RouteNames.LIBRARY,
-    component: () => import('@renderer/features/user-library/views/user-library.vue')
+    component: () => import('@renderer/features/user-library/views/user-library.vue'),
+    children: [
+      {
+        path: 'mediaplayer',
+        name: 'mediaplayer',
+        component: () => import('@renderer/features/user-library/views/media-player.vue')
+      }
+    ]
   },
   {
     path: '/community',
